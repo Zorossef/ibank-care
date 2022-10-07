c5a93cf61a9afe3032bbb71e20df29b71da7003b
Zorossef <ouhamane.y@gmail.com>
2022-10-07 15:12:54 +0100

    add components actions for wrap a col of table row

---
+++ src/components/Actions/Actions.tsx
@@ -0,0 +1,12 @@
+import Dots from "../../assets/icons/Dots";
+import { Container } from "./style";
+
+const Actions = () => {
+  return (
+    <Container>
+      <Dots />
+    </Container>
+  );
+};
+
+export default Actions;
